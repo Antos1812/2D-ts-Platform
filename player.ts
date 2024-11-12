ea6c29f460806e52d3d529656910d517e53157ec
Antek <un.tek2008@gmail.com>
2024-11-12 17:49:07 +0100

    Added file for player

---
+++ player.ts
@@ -0,0 +1,10 @@
+interface Player {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    velocityX: number;
+    velocityY: number;
+    speed: number;
+    jumping: boolean;
+}
