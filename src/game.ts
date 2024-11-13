442fd1daba3063d742b86a48a70beb45f918989e
Antek <un.tek2008@gmail.com>
2024-11-13 14:54:22 +0100

    deleted dot

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,4 @@
-import { Player } from "../player";
+import { Player } from "./player";
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
